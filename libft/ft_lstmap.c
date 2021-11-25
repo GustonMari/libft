3dd7f450d94ce8a81a014ad5c606c5dd16d634c3
Gustave MARY <gmary@e2r4p14.clusters.42paris.fr>
2021-11-25 16:53:04 +0100

    25/11 fin aprem

---
+++ libft/ft_lstmap.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmary <gmary@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/25 15:20:25 by gmary             #+#    #+#             */
+/*   Updated: 2021/11/25 15:58:25 by gmary            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	void *new_value;
+	t_list	*new_list;
+	t_list	*new_elem;
+	
+	
+	while (lst)
+	{
+		
+		
+		
+		if ()
+		lst = lst->next;
+		
+	}
+	return ;
+}
