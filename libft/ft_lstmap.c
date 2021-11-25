d85ca95c707864829ebd23ab9055d44d6b2ad2e9
Gustave MARY <gmary@e2r4p14.clusters.42paris.fr>
2021-11-25 17:49:16 +0100

    25/11 soir

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: gmary <gmary@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 15:20:25 by gmary             #+#    #+#             */
-/*   Updated: 2021/11/25 15:58:25 by gmary            ###   ########.fr       */
+/*   Updated: 2021/11/25 17:48:34 by gmary            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,51 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	void *new_value;
-	t_list	*new_list;
+	t_list	*head;
 	t_list	*new_elem;
 	
-	
+	f(lst->content)
+	head = ft_lstnew(lst->content);
+	if (!head)
+	{
+		ft_lstclear(lst, &del);
+		return (0);
+	}
+	lst = lst->next;
 	while (lst)
 	{
-		
-		
-		
-		if ()
+		f(lst->content);
+		new_elem = ft_lstnew(lst->content);
+		if (!new_elem)
+		{
+			ft_lstclear(lst, &del);
+			return (0);
+		}
+		ft_lstadd_back(head, new_elem);
 		lst = lst->next;
-		
 	}
 	return ;
+}
+
+int	main()
+{
+	t_list	*head;
+	t_list	*new2;
+	t_list	*new3;
+	t_list	*new4;
+
+	head = ft_lstnew("3");
+	new2 = ft_lstnew("2");
+	new3 = ft_lstnew("1");
+	new4 = ft_lstnew("4");
+	ft_lstadd_front(&head, new2);
+	ft_lstadd_front(&head, new3);
+	ft_lstadd_back(&head, new4);
+	ft_lstmap(head, , &ft_del);
+	while (head)
+	{
+		printf(" %s\n ", (char *)(head->content));
+		head = head->next;
+	}
+	return (0);
 }
