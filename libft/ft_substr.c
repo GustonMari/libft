cfa97787032120b6442a116376ab8fafbe5b1536
Gustave MARY <gmary@e2r4p12.clusters.42paris.fr>
2021-11-29 16:42:45 +0100

    29/11 aprem3

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,11 +6,20 @@
 /*   By: gmary <gmary@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:38:41 by gmary             #+#    #+#             */
-/*   Updated: 2021/11/29 12:04:58 by gmary            ###   ########.fr       */
+/*   Updated: 2021/11/29 16:40:00 by gmary            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static char	*ft_zero(void)
+{
+	char	*str;
+
+	str = malloc(sizeof(char) * 1);
+	str[0] = '\0';
+	return (str);
+}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -22,6 +31,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	i = 0;
 	if (!s)
 		return (NULL);
+	if (ft_strlen(s) < start)
+		return (ft_zero());
+	if (ft_strlen(&s[start]) < len)
+		len = ft_strlen(&s[start]);
 	while (i < start && s[i])
 		i++;
 	new = malloc(sizeof(char) * (len + 1));
